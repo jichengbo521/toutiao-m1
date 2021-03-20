dd877f5882b317d7d8b0c01c2701ecf6c2a826b5
小波 <941397668@qq.com>
2021-03-20 14:33:56 +0800

    项目初始化-初始化目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,16 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0, //函数参数前面要加空格
+    'space-before-blocks': 0, //函数块前面要加空格
+    'semi': 0 //分号检查
   }
 }
